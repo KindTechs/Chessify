b5cc7c0d1fe90a8afa624297b5a478c6fb41affc
Detox1127 <murcik99@gmail.com>
2023-06-05 00:35:40 +0200

    adding logic for valid pawn moves

--- Engine.py
+++ Engine.py
@@ -1,13 +1,15 @@
 """
-This file is responsible for storing all the information about the current state of a chess game. It will also be responsible
-for determining the valid moves at the current state and also keep a move log.
+This file is responsible for storing all the information about the current state of a chess game.
+It will also be responsible for determining the valid moves at the current state and also keep a move log.
 """
-class GameState():
+
+
+class GameState:
     def __init__(self):
-        #The board is an 8x8 2D list, each element of the list has 2 characters.
-        #The first character represents the color of the piece, 'b' or 'w'
-        #The second character represents the type of the piece, 'k', 'q', 'r', 'b', 'n' and 'p'
-        #"--" represents an empty space with no piece on.
+        # The board is a 8x8 2D list, each element of the list has 2 characters.
+        # The first character represents the color of the piece, 'b' or 'w'
+        # The second character represents the type of the piece, 'k', 'q', 'r', 'b', 'n' and 'p'
+        # "--" represents an empty space with no piece on.
         self.board = [
             ["br", "bn", "bb", "bq", "bk", "bb", "bn", "br"],
             ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
@@ -18,9 +20,10 @@ class GameState():
             ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
             ["wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"]
         ]
+        self.moveFunctions = {'p': self.getPawnMoves, 'r': self.getRookMoves, 'b': self.getBishopMoves,
+                              'n': self.getKnightMoves, 'q': self.getQueenMoves, 'k': self.getKingMoves}
         self.whiteToMove = True
         self.moveLog = []
-
 
     '''
     Takes a Move as a parameter and executes it
@@ -28,26 +31,24 @@ class GameState():
     def makeMove(self, move):
         self.board[move.startRow][move.startColumn] = "--"
         self.board[move.endRow][move.endColumn] = move.pieceMoved
-        self.moveLog.append(move) #log the move
-        self.whiteToMove = not self.whiteToMove #swap players
+        self.moveLog.append(move)  # log the move
+        self.whiteToMove = not self.whiteToMove  # swap players
 
     '''
     Undo the last move made.
     '''
     def undoMove(self):
-        if len(self.moveLog) != 0: #if a move was made to undo
+        if len(self.moveLog) != 0:  # if a move was made to undo
             move = self.moveLog.pop()
             self.board[move.startRow][move.startColumn] = move.pieceMoved
             self.board[move.endRow][move.endColumn] = move.pieceCaptured
-            self.whiteToMove = not self.whiteToMove #change player turns back
-
+            self.whiteToMove = not self.whiteToMove  # change player turns back
 
     '''
     All valid moves considering checks. e.g. can't move a piece if it's pinned to a king
     '''
     def getValidMoves(self):
         return self.getAllPossibleMoves()
-
 
     '''
     All moves without considering checks. e.g can move a piece even if it's pinned to a king
@@ -56,29 +57,78 @@ class GameState():
         moves = []
         for row in range(len(self.board)):
             for column in range(len(self.board[row])):
-                turn = self.board[row][column][0] #this can be "w" or "b" or "-"
-                if (turn == 'w' and self.whiteToMove) and (turn == 'b' and not self.whiteToMove):
+                turn = self.board[row][column][0]  # this can be "w" or "b" or "-"
+                if (turn == 'w' and self.whiteToMove) or (turn == 'b' and not self.whiteToMove):
                     piece = self.board[row][column][1]
-                    if piece == 'p':
-                        self.getPawnMoves(row, column, moves)
-        return  moves
-
+                    self.moveFunctions[piece](row, column, moves)  # calls the corresponding move function for each piece
+        return moves
 
     '''
-    get all the pawn moves for the pawn located at row, column and add these moves to the list
+    Get all the pawn moves for the pawn located at row, column and add these moves to the list
     '''
     def getPawnMoves(self, row, column, moves):
+        if self.whiteToMove:  # white pawn moves
+            if self.board[row - 1][column] == "--":  # 1 square pawn move
+                moves.append(Move((row, column), (row - 1, column), self.board))
+                if row == 6 and self.board[row - 2][column] == "--":  # 2 square pawn move
+                    moves.append(Move((row, column), (row - 2, column), self.board))
+            if column - 1 >= 0:  # pawn captures to the left
+                if self.board[row - 1][column - 1][0] == 'b':  # black piece to capture diagonally
+                    moves.append(Move((row, column), (row - 1, column - 1), self.board))
+            if column + 1 <= 7:  # pawn captures to the right
+                if self.board[row - 1][column + 1][0] == 'b':  # black piece to capture diagonally
+                    moves.append(Move((row, column), (row - 1, column + 1), self.board))
+
+        else:  # black pawn moves
+            if self.board[row + 1][column] == "--":  # 1 square pawn move
+                moves.append(Move((row, column), (row + 1, column), self.board))
+                if row == 1 and self.board[row + 2][column] == "--":  # 2 square pawn move
+                    moves.append(Move((row, column), (row + 2, column), self.board))
+            if column + 1 <= 7:  # pawn captures to the left
+                if self.board[row + 1][column + 1][0] == 'w':  # white piece to capture diagonally
+                    moves.append(Move((row, column), (row + 1, column + 1), self.board))
+            if column - 1 >= 0:  # pawn captures to the right
+                if self.board[row + 1][column - 1][0] == 'w':  # black piece to capture diagonally
+                    moves.append(Move((row, column), (row + 1, column - 1), self.board))
+
+    '''
+    Get all the rook moves for the pawn located at row, column and add these moves to the list
+    '''
+    def getRookMoves(self, row, column, moves):
+        pass
+
+    '''
+    Get all the bishop moves for the pawn located at row, column and add these moves to the list
+    '''
+    def getBishopMoves(self, row, column, moves):
+        pass
+
+    '''
+    Get all the knight moves for the pawn located at row, column and add these moves to the list
+    '''
+    def getKnightMoves(self, row, column, moves):
+        pass
+
+    '''
+    Get all the queen moves for the pawn located at row, column and add these moves to the list
+    '''
+    def getQueenMoves(self, row, column, moves):
+        pass
+
+    '''
+    Get all the king moves for the pawn located at row, column and add these moves to the list
+    '''
+    def getKingMoves(self, row, column, moves):
         pass
 
 
-class Move():
+class Move:
     ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4,
                    "5": 3, "6": 2, "7": 1, "8": 0}
     rowsToRanks = {value: key for key, value in ranksToRows.items()}
     filesToColumns = {"a": 0, "b": 1, "c": 2, "d": 3,
                       "e": 4, "f": 5, "g": 6, "h": 7}
     columnsToFiles = {value: key for key, value in filesToColumns.items()}
-
 
     def __init__(self, startSquare, endSquare, board):
         self.startRow = startSquare[0]
@@ -89,7 +139,6 @@ class Move():
         self.pieceCaptured = board[self.endRow][self.endColumn]
         self.moveID = self.startRow * 1000 + self.startColumn * 100 + self.endRow * 10 + self.endColumn
 
-
     '''
     Override equals method
     '''
@@ -97,7 +146,6 @@ class Move():
         if isinstance(other, Move):
             return self.moveID == other.moveID
         return False
-
 
     def getChessNotation(self):
         return self.getRankFile(self.startRow, self.startColumn) + self.getRankFile(self.endRow, self.endColumn)
--- Main.py
+++ Main.py
@@ -17,8 +17,7 @@ Initialize a global dictionary of images. This will be called only once in the m
 def loadImages():
     pieces = ['wk', 'wq', 'wb', 'wn', 'wr', 'wp', 'bk', 'bq', 'bb', 'bn', 'br', 'bp']
     for piece in pieces:
-        IMAGES[piece] = py.transform.scale(py.image.load("images/" + piece + ".png"),
-                                           (SQUARE_SIZE, SQUARE_SIZE))
+        IMAGES[piece] = py.transform.scale(py.image.load("images/" + piece + ".png"), (SQUARE_SIZE, SQUARE_SIZE))
 
 
 '''
@@ -35,41 +34,42 @@ def main():
 
     loadImages()  # load images only once
     running = True
-    squareSelected = () #keep track of the last user click in a tuple (row, column)
-    playerClicks = [] #keep track of player clicks in two tuples.
+    squareSelected = ()  # keep track of the last user click in a tuple (row, column)
+    playerClicks = []  # keep track of player clicks in two tuples.
     while running:
         for event in py.event.get():
             if event.type == py.QUIT:
                 running = False
-            #mouse handlers
+            # mouse handlers
             elif event.type == py.MOUSEBUTTONDOWN:
-                location = py.mouse.get_pos() #x,y location of the mouse
+                location = py.mouse.get_pos()  # x,y location of the mouse
                 column = location[0] // SQUARE_SIZE
                 row = location[1] // SQUARE_SIZE
-                if squareSelected == (row, column): #user clicked the same square twice
-                    squareSelected = () #deselect the square
-                    playerClicks = [] #clear the player clicks
+                if squareSelected == (row, column):  # user clicked the same square twice
+                    squareSelected = ()  # deselect the square
+                    playerClicks = []  # clear the player clicks
                 else:
                     squareSelected = (row, column)
-                    playerClicks.append(squareSelected) #append for both first and second clicks. From square a To square b
+                    playerClicks.append(
+                        squareSelected)  # append for both first and second clicks. From square a To square b
                 if len(playerClicks) == 2:
                     move = Engine.Move(playerClicks[0], playerClicks[1], gameState.board)
-                    #print(move.getChessNotation())
+                    # print(move.getChessNotation())
                     if move in validMoves:
                         gameState.makeMove(move)
                         moveMade = True
-                    squareSelected = () #reset user clicks
+                    squareSelected = ()  # reset user clicks
                     playerClicks = []
-            #key handlers
+            # key handlers
             elif event.type == py.KEYDOWN:
-                if event.key == py.K_u: #undo when 'u' is pressed
+                if event.key == py.K_u:  # undo when 'u' is pressed
                     gameState.undoMove()
                     moveMade = True
         if moveMade:
             validMoves = gameState.getValidMoves()
             moveMade = False
 
-        drawGameState(screen,gameState)
+        drawGameState(screen, gameState)
         clock.tick(MAX_FPS)
         py.display.flip()
 
